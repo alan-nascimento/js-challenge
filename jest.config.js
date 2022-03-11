de7e59a966ae8718eae214f1c46f1e3e1b2161b9
Alan Nascimento <alan.nascimento@mercadolivre.com>
2022-03-11 19:33:44 -0300

    chore: setup config

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  collectCoverageFrom: ["<rootDir>"],
+  coverageDirectory: "coverage",
+  // coverageProvider: "babel",
+  // preset: "@shelf/jest-mongodb",
+  testEnvironment: "node",
+  // transform: {
+  //   ".+\\.ts$": "ts-jest",
+  // },
+  // moduleNameMapper: {
+  //   "@/(.*)": "<rootDir>/src/$1",
+  // },
+};
